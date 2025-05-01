e436a35f7de7fad7e529230c5b293c1a4f467e1e
ToyzzBox <info@toyzzbox.com>
2025-05-01 08:41:08 +0000

    Gerekli dosyalarda değişiklik yapıldı

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
+
+<div>rüveyda</div>        <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -95,7 +97,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+         
         </a>
       </footer>
     </div>
