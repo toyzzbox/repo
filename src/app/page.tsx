b43155c872335cb364cf6656730b7b8fc12ac68d
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-09 17:10:50 +0300

    Test: README dosyası güncellendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Cart from './(frontend)/Cart'
 
 const Home = () => {
   return (
     <div>
 <h1 className='font-bold text-2xl'>En Popüler Oyuncaklar</h1>
+<Cart/>
     </div>
   )
 }
