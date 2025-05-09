09806815781232e3b9eacb3237cbdeeeaf8d6073
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-09 15:51:06 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/Header.tsx
+++ src/components/(frontend)/header/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <Logo/>
+    </div>
   )
 }
 
---
+++ src/components/(frontend)/header/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>Toyzz Box Oyuncak</div>
+  )
+}
+
+export default Logo
