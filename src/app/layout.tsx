db67cd0b08a9e68f615d3caa2f36a6ec7f7f7de6
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-09 17:00:45 +0300

    Test: README dosyası güncellendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
              <div className='flex flex-col min-h-screen'>  
               <Header/>
-        {children}
+              <main className="flex-1">{children}</main>
         <Footer/></div>
      
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-<h1>En Popüler Oyuncaklar</h1>
+<h1 className='font-bold text-2xl'>En Popüler Oyuncaklar</h1>
     </div>
   )
 }
