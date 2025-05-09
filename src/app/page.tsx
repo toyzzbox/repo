29e78531ff8c66b0a38cc768eef36d2da2a0f33f
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-09 16:56:35 +0300

    Test: README dosyası güncellendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-Deneme oluşturulmuş olmuş mu olmaz mı
+<h1>En Popüler Oyuncaklar</h1>
     </div>
   )
 }
--- src/components/(frontend)/footer/Footer.tsx
+++ src/components/(frontend)/footer/Footer.tsx
@@ -1,8 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-between'>
+        <div>
+            <h2>Kategoriler</h2>
+            <Link href="/">Oyuncaklar</Link>
+            <Link href="/">Anne & Bebek</Link>
+            <Link href="/">Okul & Kırtasiye</Link>
+            <Link href="/">Spor & Outdoor</Link>
+            <Link href="/">Hediyelik</Link>
+        </div>
+        <div>
+            <h2>Popüler Kategoriler</h2>
+            <Link href="/">Oyuncak Araba ve Setleri</Link>
+            <Link href="/">Oyuncak Bebek ve Setleri</Link>
+            <Link href="/">Bebek Oyuncakları</Link>
+            <Link href="/">Oyuncak Araba ve Setleri</Link>
+        </div>
+    </div>
   )
 }
 
