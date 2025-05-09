dbad7e59fd04fd413adf0c033b91211f2225a9a9
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-09 14:26:55 +0300

    Yaptığın değişikliklerin açıklaması

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >deneme
+      >deneme2112212121
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>denemeler burada değişiklikleri algılasın istiyorum kabul olsun
+    <div>denemeler burada değişiklikleri algılasın istiyorum kabul olsun ol
 </div>
 );
 }
