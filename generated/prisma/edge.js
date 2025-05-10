a43be9aabde2d925b857741297a7b6a12362fbed
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-10 18:52:48 +0300

    Test: README dosyası güncellendi

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -125,7 +125,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -126,7 +126,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
