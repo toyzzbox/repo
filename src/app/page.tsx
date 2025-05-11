3ec1cf8d9411d32651a0829ef7b7ddc95d6d2cce
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-11 12:52:13 +0300

    Test: README dosyası güncellendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,30 @@ export default async function Home() {
           </p>
         )}
       </div>
+      <h1 className="text-2xl font-bold text-center p-5">Flaş İndirimler</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <p className="text-center col-span-full text-gray-500">
+            Hiç ürün bulunamadı.
+          </p>
+        )}
+      </div>
+      <h1 className="text-2xl font-bold text-center p-5">En Yeni Gelenler</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <p className="text-center col-span-full text-gray-500">
+            Hiç ürün bulunamadı.
+          </p>
+        )}
+      </div>
     </main>
   );
 }
