5b0e8cb83e8c75fa4855d2651c2b21af0a29c708
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-12 18:56:33 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/product/ProductCard.tsx
+++ src/components/(frontend)/product/ProductCard.tsx
@@ -1,14 +1,40 @@
-type Product = {
-  id: string;
-  name: string;
-  price: number;
+"use client"
+
+import { formatPrice } from "@/utils/formatPrice";
+import { useRouter } from "next/navigation";
+
+type ProductCardProps = {
+  product: {
+    name: string;
+    slug?: string | null;
+
+    price: number;
+  };
 };
 
-export default function ProductCard({ product }: { product: Product }) {
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (product.slug) {
+      router.push(`/product/${product.slug}`);
+    }
+  };
+
+  // Determine the image URL based on the product's URLs array
+ 
+
   return (
-    <div className="border p-4 rounded shadow">
-      <h2 className="font-semibold text-lg">{product.name}</h2>
-      <p className="text-gray-600">{product.price}₺</p>
+    <div 
+      className="border rounded-lg shadow-lg p-4 cursor-pointer" 
+      onClick={handleClick}
+    >
+      resim
+      <div className="text-center mt-4">
+        <h3 className="text-lg font-semibold">{product.name}</h3>
+        <h3 className="text-lg font-semibold">{formatPrice(product.price)}</h3>      </div>
     </div>
   );
-}
+};
+
+export default ProductCard;
