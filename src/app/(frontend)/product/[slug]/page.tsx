64f93003aa86555e6acc962302d324dffe9e4762
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-12 18:53:08 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/product/[slug]/page.tsx
+++ src/app/(frontend)/product/[slug]/page.tsx
@@ -1,32 +1,27 @@
-
 import ProductDetails from "@/components/(frontend)/product/ProductDetails";
 import { prisma } from "@/lib/prisma";
 
+type PageProps = {
+  params: Promise<{ slug: string }>;
+};
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
-  // params'ı doğrudan kullanıyoruz
-  const { slug } = params;
+export default async function ProductPage({ params }: PageProps) {
+  const { slug } = await params;
 
-  // Ürünü veritabanından alıyoruz
   const product = await prisma.product.findUnique({
-    where: {
-      slug: slug,
-    },
+    where: { slug },
     select: {
       id: true,
-      slug: true, // Slug'u da seçiyoruz
+      slug: true,
       name: true,
       description: true,
       price: true,
-     
     },
   });
 
-  // Ürün bulunamazsa hata mesajı döndürüyoruz
   if (!product) {
     return <div>Product not found</div>;
   }
 
-  // Ürünü ProductDetails bileşenine geçiriyoruz
   return <ProductDetails product={product} />;
 }
