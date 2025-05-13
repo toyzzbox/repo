bc85c2dcf7981f9567a934745a9a85b3e36eecdb
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-13 17:45:23 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/MobileFooter.tsx
+++ src/components/(frontend)/header/MobileFooter.tsx
@@ -1,95 +1,62 @@
+"use client";
+
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
-import Link from "next/link"
-   
-  export default function Mobile() {
-    return (
-  
-                    <div> <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Tüm Kategoriler</AccordionTrigger>
-       
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Link from "next/link";
+
+export default function MobileFooter() {
+  return (
+    <div className="block md:hidden px-4 py-6 bg-gray-100 border-t">
+      <Accordion type="single" collapsible className="w-full space-y-2">
+        {/* Tüm Kategoriler */}
+        <AccordionItem value="categories">
+          <AccordionTrigger className="font-semibold text-left">
+            Tüm Kategoriler
+          </AccordionTrigger>
           <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Oyuncaklar</Link>  
+            <div className="flex flex-col space-y-2 font-medium text-sm">
+              <Link href="/">Oyuncaklar</Link>
+              <Link href="/">Anne &amp; Bebek</Link>
+              <Link href="/">Spor & Outdoor</Link>
+              <Link href="/">Okul & Kırtasiye</Link>
+              <Link href="/">Fırsatlar</Link>
             </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Anne & Bebek</Link>  
-            </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Spor & Outdoor</Link>  
-            </div>
-                 </AccordionContent>
-                 
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Fırsatlar</Link>  
-            </div>
-                 </AccordionContent>
+          </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Popüler Markalar</AccordionTrigger>
-         
+
+        {/* Popüler Markalar */}
+        <AccordionItem value="brands">
+          <AccordionTrigger className="font-semibold text-left">
+            Popüler Markalar
+          </AccordionTrigger>
           <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Oyuncaklar</Link>  
+            <div className="flex flex-col space-y-2 font-medium text-sm">
+              <Link href="/">Lego</Link>
+              <Link href="/">Fisher-Price</Link>
+              <Link href="/">Hot Wheels</Link>
+              <Link href="/">Barbie</Link>
             </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Anne & Bebek</Link>  
-            </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Spor & Outdoor</Link>  
-            </div>
-                 </AccordionContent>
-                 
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Fırsatlar</Link>  
-            </div>
-                 </AccordionContent>
+          </AccordionContent>
         </AccordionItem>
-     
-        <AccordionItem value="item-3">
-          <AccordionTrigger>Popüler Markalar</AccordionTrigger>
-         
+
+        {/* Hakkımızda */}
+        <AccordionItem value="about">
+          <AccordionTrigger className="font-semibold text-left">
+            Hakkımızda
+          </AccordionTrigger>
           <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Oyuncaklar</Link>  
+            <div className="flex flex-col space-y-2 font-medium text-sm">
+              <Link href="/kurumsal">Kurumsal</Link>
+              <Link href="/iletisim">İletişim</Link>
+              <Link href="/kariyer">Kariyer</Link>
             </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Anne & Bebek</Link>  
-            </div>
-                 </AccordionContent>
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Spor & Outdoor</Link>  
-            </div>
-                 </AccordionContent>
-                 
-                 <AccordionContent>
-          <div className="flex flex-col space-y-2 font-bold">
-          <Link href="/">Fırsatlar</Link>  
-            </div>
-                 </AccordionContent>
+          </AccordionContent>
         </AccordionItem>
       </Accordion>
-
-     
-        </div>
-     
-    )
-  }
+    </div>
+  );
+}
