74e6d159b42cd2eb4b3fac231d617ed1cee279ee
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-13 18:20:31 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/cart/ShoppingCart.tsx
+++ src/app/(frontend)/cart/ShoppingCart.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
-import { decrementQuantity, getCart, incrementQuantity, removeFromTheCart } from "@/redux/cartSlice";
+import {
+  decrementQuantity,
+  getCart,
+  incrementQuantity,
+  removeFromTheCart,
+} from "@/redux/cartSlice";
 import Image from "next/image";
 import React from "react";
 
@@ -18,11 +23,10 @@ export const ShoppingCart = () => {
   const cart = useAppSelector(getCart) as CartItem[];
   const dispatch = useAppDispatch();
 
-  let totalPrice = 0;
-
-  cart.forEach((item) => {
-    totalPrice += item.price * item.quantity;
-  });
+  const totalPrice = cart.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
   return (
     <div className="w-[80%] p-5">
@@ -32,75 +36,73 @@ export const ShoppingCart = () => {
       </div>
 
       {cart.length > 0 ? (
-        cart.map((product, index) => {
-          return (
-            <div
-              className="mt-4 flex justify-between items-center border-b border-gray-200 py-4"
-              key={`${product.id || product.slug}-${index}`}
-            >
-              <div className="flex items-center">
-                {product?.urls && product.urls.length > 0 ? (
-                  <Image
-                    src={product.urls[0]}
-                    width={100}
-                    height={100}
-                    alt={product?.name || "Ürün görseli"}
-                    className="rounded-md"
-                  />
-                ) : (
-                  <div className="w-24 h-24 bg-gray-300 flex items-center justify-center rounded-md">
-                    <span className="text-sm text-gray-600">No Image</span>
-                  </div>
-                )}
-                <div className="ml-4">
-                  <h1 className="font-medium text-lg">{product?.name}</h1>
-                  <p className="text-green-500 text-sm">In Stock</p>
-                  <h1
-                    onClick={() => dispatch(removeFromTheCart(product.slug))}
-                    className="font-bold text-red-600 cursor-pointer mt-2"
-                  >
-                    Sil
-                  </h1>
+        cart.map((product, index) => (
+          <div
+            className="mt-4 flex justify-between items-center border-b border-gray-200 py-4"
+            key={`${product.id || product.slug}-${index}`}
+          >
+            <div className="flex items-center">
+              {product.urls && product.urls.length > 0 ? (
+                <Image
+                  src={product.urls[0]}
+                  width={100}
+                  height={100}
+                  alt={product.name}
+                  className="rounded-md"
+                />
+              ) : (
+                <div className="w-24 h-24 bg-gray-300 flex items-center justify-center rounded-md">
+                  <span className="text-sm text-gray-600">No Image</span>
                 </div>
-              </div>
-              <div className="flex items-center text-lg font-medium rounded-md bg-gray-200 px-4 py-1 mt-2">
-                <div
-                  onClick={() => {
-                    if (product.quantity > 1) {
-                      dispatch(decrementQuantity(product));
-                    }
-                  }}
-                  className="cursor-pointer mr-4"
+              )}
+              <div className="ml-4">
+                <h1 className="font-medium text-lg">{product.name}</h1>
+                <p className="text-green-500 text-sm">Stokta</p>
+                <h1
+                  onClick={() => dispatch(removeFromTheCart(product.slug))}
+                  className="font-bold text-red-600 cursor-pointer mt-2"
                 >
-                  -
-                </div>
-                <span>{product.quantity}</span>
-                <div
-                  onClick={() => {
-                    dispatch(incrementQuantity(product));
-                  }}
-                  className="cursor-pointer ml-4"
-                >
-                  +
-                </div>
-              </div>
-              <div className="text-right">
-                <h1 className="font-bold text-2xl">{`$${product.price}`}</h1>
-                <p className="text-xs text-gray-500">
-                  TL: <span className="line-through">indirimli fiyat</span>
-                </p>
+                  Sil
+                </h1>
               </div>
             </div>
-          );
-        })
+            <div className="flex items-center text-lg font-medium rounded-md bg-gray-200 px-4 py-1 mt-2">
+              <div
+                onClick={() => {
+                  if (product.quantity > 1) {
+                    dispatch(decrementQuantity(product));
+                  }
+                }}
+                className="cursor-pointer mr-4"
+              >
+                -
+              </div>
+              <span>{product.quantity}</span>
+              <div
+                onClick={() => {
+                  dispatch(incrementQuantity(product));
+                }}
+                className="cursor-pointer ml-4"
+              >
+                +
+              </div>
+            </div>
+            <div className="text-right">
+              <h1 className="font-bold text-2xl">{`₺${product.price}`}</h1>
+              <p className="text-xs text-gray-500">
+                TL: <span className="line-through">indirimli fiyat</span>
+              </p>
+            </div>
+          </div>
+        ))
       ) : (
         <div className="text-center py-10">
           <h1 className="text-gray-500 text-xl">Sepetiniz boş!</h1>
         </div>
       )}
       <h1 className="text-right">
-        {`Subtotal (${cart.length} items): `}
-        <span className="font-bold">{totalPrice}</span>
+        {`Toplam (${cart.length} ürün): `}
+        <span className="font-bold">₺{totalPrice.toFixed(2)}</span>
       </h1>
     </div>
   );
