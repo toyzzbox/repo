785569ab653469539b47b37dbaabc06590f8a504
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-14 18:25:59 +0300

    Test: README dosyası güncellendi

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,23 @@
-import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "auth"
+import type { AuthConfig } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export default {providers: [Google]} satisfies NextAuthConfig;
+export const authConfig: AuthConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }) as any,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        return null;
+      },
+    }) as any,
+  ],
+  session: { strategy: "jwt" },
+};
