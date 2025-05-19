64fb30873950304e162dc774885136e50cf19a17
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-19 14:48:53 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/add-category/action.ts
+++ src/app/(admin)/administor/add-category/action.ts
@@ -2,33 +2,22 @@
 
 import { prisma } from "@/lib/prisma";
 
-
-// CreateCategoryArgs tipini tanımlama
-type CreateCategoryArgs = {
-  name: string;
-  slug: string;
-  description: string;
-  parentId: string | null; // parentId null olabilir
-}
-
 // createCategory fonksiyonu
-export async function createCategory(previousState: any, formData: FormData) {
+export async function createCategory(_previousState: unknown, formData: FormData) {
   try {
     // FormData'dan değerleri al
     const name = formData.get("name") as string;
     const slug = formData.get("slug") as string;
     const description = formData.get("description") as string;
     const parentId = formData.get("parentId") as string | null; // parentId null olabilir
- 
-  
+
     // Kategori oluşturma
     await prisma.category.create({
       data: {
         slug,
         name,
         description,
-        parentId: parentId || null, // If parentId is null, pass null
-
+        parentId: parentId || null,
       },
     });
 
