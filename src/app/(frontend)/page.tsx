ed2c7afb17c4fb5d9af81a82de63d5ccf18e16f7
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-19 21:59:37 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,12 +1,16 @@
+import { getProducts } from "@/actions/getProduct";
+import ProductCard from "@/components/(frontend)/product/ProductCard";
 
 export default async function Home() {
-  // const products = await getProducts();
-
+  const products = await getProducts();
   return (
     <main className="m-2">
       <h1 className="text-2xl font-bold text-center p-5">En Popüler Ürünler</h1>
-      <main className="m-2">
-deneme</main>
+     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+         {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>  
     </main>
   );
 }
