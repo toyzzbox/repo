68290acb4fdd32b4614242021c8c37c00c64177a
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-23 10:48:46 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/add-product/page.tsx
+++ src/app/(admin)/administor/add-product/page.tsx
@@ -1,8 +1,9 @@
+import { prisma } from "@/lib/prisma";
 import ProductForm from "./ProductForm";
 
 export default async function Page() {
+  const brands = await prisma.brand.findMany();
+  const safeBrands = JSON.parse(JSON.stringify(brands)); // serialize edilmeye uygun hale getir
 
-    return (
-        <ProductForm/> // ProductForm kullan ve media verisini ilet
-    );
+  return <ProductForm brands={safeBrands} />;
 }
