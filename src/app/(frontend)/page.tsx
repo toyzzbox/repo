1c94da7b99d022f38aec4430e0d71ed3dafd04a7
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-24 23:54:08 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,12 +2,10 @@ import { getBrands } from "@/actions/getBrands";
 import { getCategories } from "@/actions/getCategories";
 import { getProducts } from "@/actions/getProduct";
 import { BrandCard } from "@/components/(frontend)/product/BrandCard";
-import CategoryCard from "@/components/(frontend)/product/CategoryCard";
 import { ProductCard } from "@/components/(frontend)/product/ProductCard";
 
 export default async function Home() {
   const products = await getProducts();
-  const categories = await getCategories()
   const brands = await getBrands();
   return (
     <main className="m-2">
