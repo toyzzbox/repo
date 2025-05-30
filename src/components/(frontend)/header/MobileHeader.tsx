3d16562a83a098327f3ccb009a13bcf5197a137e
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-30 20:04:09 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/MobileHeader.tsx
+++ src/components/(frontend)/header/MobileHeader.tsx
@@ -8,27 +8,25 @@ import HamburgerMenu from './HamburgerMenu';
 import CartCountMobile from './CartCountMobile';
 import UserMobileMenu from './UserMobileMenu';
 
-export default async function MobileHeader () {
-
-
-
+export default async function MobileHeader() {
   return (
-    <div className='md:hidden'>
-       
-       <div className="flex justify-between items-center">
-  <HamburgerMenu />
-  <Logo />
-  <div className='flex items-center'>
-    <UserMobileMenu/>
-  <CartCountMobile/>
-  </div>
-
-</div>
-
-        <div className='flex items-center justify-center'>
-        <SearchBar />
+    <div className="md:hidden">
+      {/* Sabit üst header */}
+      <div className="fixed top-0 left-0 right-0 bg-white z-50 shadow-md">
+        <div className="flex justify-between items-center px-4 py-2">
+          <HamburgerMenu />
+          <Logo />
+          <div className="flex items-center gap-4 m-2">
+            <UserMobileMenu />
+            <CartCountMobile />
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
 
+      {/* Arama çubuğu (sabit değil) */}
+      <div className="flex items-center justify-center mt-20 px-4">
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
