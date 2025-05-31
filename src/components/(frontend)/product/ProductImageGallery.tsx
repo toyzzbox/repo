2d8346bce5ea0616f4b90265859f9fa856a1e4ea
toyzzbox <mehmetonurtass02@gmail.com>
2025-05-31 11:06:21 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/product/ProductImageGallery.tsx
+++ src/components/(frontend)/product/ProductImageGallery.tsx
@@ -1,46 +1,60 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
-type ProductImageGalleryProps = {
+
+type Props = {
   images: string[];
   productName: string;
 };
 
-export default function ProductImageGallery({ images, productName }: ProductImageGalleryProps) {
+export default function ProductImageGallery({ images, productName }: Props) {
   const [selectedImage, setSelectedImage] = useState(images?.[0]);
 
   return (
     <div className="flex flex-col gap-4">
-      <div>
-        {selectedImage ? (
+      {/* Shadcn Modal Trigger */}
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="cursor-zoom-in">
+            <AspectRatio ratio={16 / 9}>
+              <img
+                src={selectedImage}
+                alt={productName}
+                className="rounded-lg w-full h-full object-contain border transition-transform hover:scale-105 duration-200"
+              />
+            </AspectRatio>
+          </div>
+        </DialogTrigger>
+
+        {/* Modal içeriği */}
+        <DialogContent className="max-w-4xl p-0 bg-black">
           <img
             src={selectedImage}
             alt={productName}
-            className="rounded-lg w-full max-h-[400px] object-contain border"
+            className="rounded-lg w-full h-[80vh] object-contain"
           />
-        ) : (
-          <div className="w-full h-64 bg-gray-200 flex items-center justify-center text-gray-500">
-            No Image Available
-          </div>
-        )}
-      </div>
+        </DialogContent>
+      </Dialog>
 
-      {images.length > 1 && (
-        <div className="flex gap-2 overflow-x-auto">
-          {images.map((img, index) => (
-            <img
-              key={index}
-              src={img}
-              alt={`Thumbnail ${index + 1}`}
-              className={`h-20 w-20 object-cover rounded border cursor-pointer ${
-                selectedImage === img ? 'border-orange-500 ring-2 ring-orange-400' : 'border-gray-300'
-              }`}
-              onClick={() => setSelectedImage(img)}
-            />
-          ))}
-        </div>
-      )}
+      {/* Thumbnail'lar */}
+      <div className="flex gap-2 overflow-x-auto">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Thumbnail ${index + 1}`}
+            onClick={() => setSelectedImage(img)}
+            className={`h-20 w-20 object-cover rounded-md border cursor-pointer transition-all ${
+              selectedImage === img
+                ? 'border-orange-500 ring-2 ring-orange-400'
+                : 'border-gray-300'
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
