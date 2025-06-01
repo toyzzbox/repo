0fa087b2ec6e2a07f618de9fce3a3ea53ee9534d
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-01 13:07:44 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/UserMobileMenu.tsx
+++ src/components/(frontend)/header/UserMobileMenu.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-
+import { useRouter } from "next/navigation";
 import { UserRound } from "lucide-react";
 
 const UserMobileMenu = () => {
+  const router = useRouter();
 
+  const handleClick = () => {
+    router.push("/login"); // Giriş sayfasının yolu burada
+  };
 
   return (
     <div
+      onClick={handleClick}
       className="relative cursor-pointer flex items-center"
     >
       <div className="text-3xl">
         <UserRound />
       </div>
-     
     </div>
   );
 };
