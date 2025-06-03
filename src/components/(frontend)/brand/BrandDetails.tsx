656faab94ce3b796327b129294fefd168cef551f
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-03 15:13:19 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/brand/BrandDetails.tsx
+++ src/components/(frontend)/brand/BrandDetails.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { ProductCard } from "../product/ProductCard";
+import  ProductCard  from "../product/ProductCard";
 import FilterSidebar from "./FilterSidebar";
 import SortSelect, { SortOption } from "./SortSelect";
 
