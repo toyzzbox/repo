587904dc171366c7c40ec6eebc35e07eb47681c5
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-03 11:44:47 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/product/ProductDetailTab.tsx
+++ src/components/(frontend)/product/ProductDetailTab.tsx
@@ -10,24 +10,25 @@ interface ProductDetailTabsProps {
 
 export function ProductDetailTabs({ description, comments, questions }: ProductDetailTabsProps) {
   return (
-<Tabs defaultValue="description" className="w-full mt-6">
-  <TabsList className="w-full flex flex-col sm:flex-row justify-start border-b">
-    <TabsTrigger value="description">Açıklama</TabsTrigger>
-    <TabsTrigger value="comments">Yorumlar</TabsTrigger>
-    <TabsTrigger value="questions">Sorular</TabsTrigger>
-  </TabsList>
+    <Tabs defaultValue="description" className="w-full mt-6">
+      <TabsList className="w-full flex justify-start border-b sm:flex flex-col gap-4 text-balance">
 
-  <TabsContent value="description">
-    <div className="mt-4 text-sm text-gray-700 whitespace-pre-line">{description}</div>
-  </TabsContent>
+        <TabsTrigger value="description">Açıklama</TabsTrigger>
+        <TabsTrigger value="comments">Yorumlar</TabsTrigger>
+        <TabsTrigger value="questions">Sorular</TabsTrigger>
+      </TabsList>
 
-  <TabsContent value="comments">
-    <div className="mt-4">{comments}</div>
-  </TabsContent>
+      <TabsContent value="description">
+        <div className="mt-4 text-sm text-gray-700 whitespace-pre-line">{description}</div>
+      </TabsContent>
 
-  <TabsContent value="questions">
-    <div className="mt-4">{questions}</div>
-  </TabsContent>
-</Tabs>
+      <TabsContent value="comments">
+        <div className="mt-4">{comments}</div>
+      </TabsContent>
+
+      <TabsContent value="questions">
+        <div className="mt-4">{questions}</div>
+      </TabsContent>
+    </Tabs>
   );
 }
