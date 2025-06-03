88c1bacd55e64fb4761d10901ec2f9fc09657147
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-03 16:43:43 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/UserMenu.tsx
+++ src/components/(frontend)/header/UserMenu.tsx
@@ -1,48 +1,66 @@
+"use client";
+
 import { LogOut, Settings, User } from "lucide-react";
 import Link from "next/link";
-
+import { signOut, useSession } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function UserMenu() {
+  const { data: session } = useSession();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size="icon" className="flex items-center gap-2 py-2 px-4 rounded">
           <User />
-          Giriş Yap
+          {session?.user ? session.user.name?.split(" ")[0] || "Hesabım" : "Giriş Yap"}
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>user</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem asChild>
-            <Link href="/settings">
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-            </Link>
-          </DropdownMenuItem>
-          {/* TODO: Show this only for admins */}
-          {/* <DropdownMenuItem asChild>
-                <Link href="/admin">
-                  <Lock className="mr-2 h-4 w-4" />
-                  Admin
+        {session?.user ? (
+          <>
+            <DropdownMenuLabel>{session.user.email}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild>
+                <Link href="/settings">
+                  <Settings className="mr-2 h-4 w-4" />
+                  <span>Ayarlar</span>
                 </Link>
-              </DropdownMenuItem> */}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <form action={async () => {
-            "use server";
-            // await signOut();
-          }}>
-            <button className="flex w-full items-center">
-              <LogOut className="mr-2 h-4 w-4" /> Sign Out
-            </button>
-          </form>
-        </DropdownMenuItem>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => signOut({ callbackUrl: "/" })}
+              className="cursor-pointer"
+            >
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Çıkış Yap</span>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuLabel>Hoş geldiniz</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href="/login">Giriş Yap</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/register">Üye Ol</Link>
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
