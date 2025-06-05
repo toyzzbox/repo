8a8d8ad042ad1429d930517b8223cc840bf8e8d7
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-05 17:58:13 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/layout.tsx
+++ src/app/(admin)/administor/layout.tsx
@@ -1,17 +1,7 @@
-import "@/app/globals.css";
+// app/(admin)/layout.tsx
+
 import Sidebar from "@/components/(backend)/Sidebar";
 import Topbar from "@/components/(backend)/Topbar";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function AdminLayout({
   children,
@@ -19,18 +9,14 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="tr">
-      <body className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 text-gray-900`}>
-        <div>
-          <Topbar />
-          <div className="flex min-h-screen">
-            <aside className="w-1/6 bg-gray-800 p-4 text-white">
-              <Sidebar />
-            </aside>
-            <main className="flex-1 p-6">{children}</main>
-          </div>
-        </div>
-      </body>
-    </html>
+    <div>
+      <Topbar />
+      <div className="flex min-h-screen">
+        <aside className="w-1/6 bg-gray-800 p-4 text-white">
+          <Sidebar />
+        </aside>
+        <main className="flex-1 p-6 bg-gray-100 text-gray-900">{children}</main>
+      </div>
+    </div>
   );
 }
