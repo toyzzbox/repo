76086788e3588a47551a6342335b59e2b05acf7f
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-05 18:35:39 +0300

    Test: README dosyası güncellendi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{ts,tsx}",
+    "./app/(frontend)/**/*.{ts,tsx}",
+    "./app/(backend)/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./pages/**/*.{ts,tsx}",      // Eğer pages dizini kullanıyorsan
     "./src/**/*.{ts,tsx}",        // Eğer başka klasörlerde component varsa
