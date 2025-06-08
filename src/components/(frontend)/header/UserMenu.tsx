c746321ca818eefcf775a2842769af3a5e079e86
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-08 11:59:49 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/UserMenu.tsx
+++ src/components/(frontend)/header/UserMenu.tsx
@@ -25,7 +25,7 @@ export default async function UserMenu() {
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent className="w-56 bg-white">
+      <DropdownMenuContent className="w-56 bg-white text-black">
         {session?.user ? (
           <>
             <DropdownMenuLabel>{session.user.email}</DropdownMenuLabel>
