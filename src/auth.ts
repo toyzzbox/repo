aab66def17d47402d6d3b0a69d37a9a0a935a4ea
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-15 10:35:59 +0300

    Test: README dosyası güncellendi

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config"; // ✅ default export olmalı
+import authConfig from "./auth.config"; // ✅ default export kullanılmalı
 
 export const {
-  handlers: { GET, POST },
+  handlers: { GET, POST }, // ✅ bunu export etmeliyiz
   auth,
   signIn,
   signOut,
-} = NextAuth(authConfig); // ✅ tekrar adapter verme!
+} = NextAuth(authConfig);
