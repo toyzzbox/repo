be1bd072785474a9ea0108c2b055c907c4700066
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-21 10:08:11 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/nitelik/page.tsx
+++ src/app/(admin)/administor/nitelik/page.tsx
@@ -1,24 +1,39 @@
-
 import { prisma } from "@/lib/prisma";
 import CreateAttributeForm from "./CreateAttributeForm";
 
-async function getAttributes() {
-    try {
-        const attributes = await prisma.attributeGroup.findMany();
-        return attributes;
-    } catch (error) {
-        console.error("Error fetching attributes:", error);
-        return [];
-    }
+async function getAttributeGroups() {
+  try {
+    return await prisma.attributeGroup.findMany();
+  } catch (error) {
+    console.error("Error fetching attribute groups:", error);
+    return [];
+  }
+}
+
+async function getMedias() {
+  try {
+    return await prisma.media.findMany({
+      select: {
+        id: true,
+        urls: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching medias:", error);
+    return [];
+  }
 }
 
 export default async function CreateAttribute() {
-    const attributeGroups = await getAttributes(); // Değişken adı düzeltildi
+  const [attributeGroups, medias] = await Promise.all([
+    getAttributeGroups(),
+    getMedias(),
+  ]);
 
-    return (
-        <div className='w-full max-w-6xl mx-auto text-center h-screen'>
-            <h1 className='font-bold text-2xl mt-20 mb-10'>Create New Category</h1>
-            <CreateAttributeForm attributeGroups={attributeGroups} /> {/* Prop adı düzeltildi */}
-        </div>
-    );
+  return (
+    <div className="w-full max-w-6xl mx-auto text-center h-screen">
+      <h1 className="font-bold text-2xl mt-20 mb-10">Yeni Nitelik Oluştur</h1>
+      <CreateAttributeForm attributeGroups={attributeGroups} medias={medias} />
+    </div>
+  );
 }
