4af7e488fa288efbdcf396aed2aba9a1e52a3143
toyzzbox <mehmetonurtass02@gmail.com>
2025-06-24 20:43:44 +0300

    Test: README dosyası güncellendi

--- src/actions/createMedia.ts
+++ src/actions/createMedia.ts
@@ -1,10 +1,15 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { MediaType } from '@prisma/client';
 
 export async function createMedia(urls: string[]) {
   const media = await prisma.media.create({
-    data: { urls },
+    data: {
+      urls,
+      type: MediaType.image, // veya MediaType.video
+    },
   });
+
   return media;
 }
