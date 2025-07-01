496f0916f8f21370a91f3bc290851da0878a4af5
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-01 22:57:29 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/categories/[...slug]/page.tsx
+++ src/app/(frontend)/categories/[...slug]/page.tsx
@@ -127,7 +127,7 @@ export default async function CategoryPage({
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-semibold mb-6">{category.name}</h1>
-<div>
+<div className="flex">
 <SortSelect />
       <MobileFilterButton
       subcategories={subcategories}
