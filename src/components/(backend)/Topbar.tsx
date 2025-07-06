dfbfffdc33cd64c21e9c4d651fefecc6052c0e72
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-06 12:27:03 +0300

    Test: README dosyası güncellendi

--- src/components/(backend)/Topbar.tsx
+++ src/components/(backend)/Topbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Topbar = () => {
   return (
     <div className='flex justify-between items-center bg-gray-600 p-2'>
+      <div><Sidebar/></div>
         <div className='text-2xl p-2 text-white'>Toyzz Box</div>
         <div className='text-lg text-white'>
             Çıkış Yap
