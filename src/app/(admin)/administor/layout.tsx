ffbe890ea2c1fde43a2de61dffe0abbd5b724396
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-06 12:31:49 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/layout.tsx
+++ src/app/(admin)/administor/layout.tsx
@@ -1,6 +1,3 @@
-// app/(admin)/layout.tsx
-
-import Sidebar from "@/components/(backend)/Sidebar";
 import Topbar from "@/components/(backend)/Topbar";
 
 export default function AdminLayout({
@@ -9,14 +6,14 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-     <Sidebar />
-      <Topbar />    
-      <div className="flex min-h-screen">
+    <div className="flex min-h-screen">
+    
 
-      
-
-        <main className="flex-1 p-6 bg-gray-100 text-gray-900">{children}</main>
+      <div className="flex-1 flex flex-col">
+        <Topbar /> {/* Topbar üst kısmı */}
+        <main className="flex-1 p-6 bg-gray-100 text-gray-900">
+          {children} {/* İçerik buraya gelir */}
+        </main>
       </div>
     </div>
   );
