f751b44b42f4e20c944db561ddfe64b8c7e68659
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-06 12:57:01 +0300

    Test: README dosyası güncellendi

--- src/components/(backend)/Topbar.tsx
+++ src/components/(backend)/Topbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Topbar = () => {
   return (
-    <div className='flex justify-end items-center bg-gray-600 p-2'>
+    <div className='flex justify-end items-center bg-gray-800 p-2'>
 
         <div className='text-sm text-white'>
             Çıkış Yap
