08b06b0f31639cd539b4c0cc2a7c1ae15c89fcb7
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-18 11:40:28 +0300

    Test: README dosyası güncellendi

--- src/actions/getDiscountedProducts.ts
+++ src/actions/getDiscountedProducts.ts
@@ -1,40 +1,29 @@
-"use server";
-
 import { prisma } from "@/lib/prisma";
-import { Product } from "@/types/products";
 
-/**
- * En son eklenen 20 indirimli ürünü getirir.
- * İndirimli ürün = discount > 0 && discount < price
- */
 export async function getDiscountedProducts() {
-  const products: Product[] = await prisma.product.findMany({
+  const products = await prisma.product.findMany({
     where: {
       discount: {
         not: null,
-        gt: 0
+        gt: 0,
       },
       price: {
-        gt: 0
-      }
-    },
-    group: {
-      select: {
-        name: true,
+        gt: 0,
       },
     },
     orderBy: {
-      createdAt: "desc"
+      createdAt: "desc",
     },
     take: 20,
     include: {
       medias: true,
-      group: true
-    }
+      group: {
+        select: {
+          name: true,
+        },
+      },
+    },
   });
-  
-  const validProducts = products.filter(p => p.discount < p.price);
 
   return products;
 }
-
