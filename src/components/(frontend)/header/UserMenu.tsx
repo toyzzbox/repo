d0e5df8511bf24f96d84caa79e646484a000bac9
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-22 13:32:14 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/UserMenu.tsx
+++ src/components/(frontend)/header/UserMenu.tsx
@@ -20,7 +20,7 @@ export default async function UserMenu() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size="logo" variant={"user"} className="flex items-center gap-2 py-2 px-4 rounded">
-          <User />
+        <User className="w-6 h-6" />
           {session?.user?.name?.split(" ")[0] ?? "Giriş Yap"}
         </Button>
       </DropdownMenuTrigger>
