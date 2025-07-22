f641443d133f77391f3e5e910f678557b18b7769
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-22 19:32:22 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/login/page.tsx
+++ src/app/(frontend)/login/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { auth } from "@/auth";
 import LoginForm from "@/components/(frontend)/auth/login-form";
 import { redirect } from "next/navigation";
