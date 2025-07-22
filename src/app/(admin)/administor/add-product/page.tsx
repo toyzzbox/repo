2d2891c97e833650ab286f04290fca2a2acb540e
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-22 12:19:52 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/add-product/page.tsx
+++ src/app/(admin)/administor/add-product/page.tsx
@@ -2,15 +2,16 @@ import { prisma } from "@/lib/prisma";
 import ProductForm from "./ProductForm";
 
 export default async function Page() {
-  const [brands, categories, medias, productGroups] = await Promise.all([
+  const [brands, categories, medias, productGroups, attributes] = await Promise.all([
     prisma.brand.findMany(),
     prisma.category.findMany(),
     prisma.media.findMany({
       orderBy: {
-        createdAt: "desc", // 🔥 En son yüklenen medyalar en başta
+        createdAt: "desc",
       },
     }),
     prisma.productGroup.findMany(),
+    prisma.attribute.findMany(), // ✅ düzeltildi
   ]);
 
   const serialize = (data: any) => JSON.parse(JSON.stringify(data));
@@ -19,8 +20,9 @@ export default async function Page() {
     <ProductForm
       brands={serialize(brands)}
       categories={serialize(categories)}
-      medias={serialize(medias)} // ✅ Artık sıralı geliyor
+      medias={serialize(medias)}
       productGroups={serialize(productGroups)}
+      attributes={serialize(attributes)}
     />
   );
 }
