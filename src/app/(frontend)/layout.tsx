add8dcbf99d1fc6b5adffc369e579e1015e282c0
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-28 10:39:35 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -5,22 +5,24 @@ import { ReduxProvider } from "@/components/providers/ReduxProvider";
 import PageContainer from "@/components/(frontend)/container/PageContainer";
 import { Toaster } from "sonner";
 import GtagClient from "@/components/(frontend)/analytics/GtagClient";
-
+import SessionProviderWrapper from "@/components/providers/SessionProviderWrapper";
 export default function FrontendLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <GtagClient /> {/* sadece bu client */}
+      <GtagClient />
       <ReduxProvider>
-        <Toaster position="top-right" richColors />
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1">
-            <PageContainer>{children}</PageContainer>
-          </main>
-          <footer>
-            <Footer />
-          </footer>
-        </div>
+        <SessionProviderWrapper> {/* ✅ sadece bir tane ve düzgün kapatılmış */}
+          <Toaster position="top-right" richColors />
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">
+              <PageContainer>{children}</PageContainer>
+            </main>
+            <footer>
+              <Footer />
+            </footer>
+          </div>
+        </SessionProviderWrapper>
       </ReduxProvider>
     </>
   );
