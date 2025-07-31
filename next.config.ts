fde669a661a88fc0b5311a6e492f9305918e4e4f
toyzzbox <mehmetonurtass02@gmail.com>
2025-07-31 12:31:47 +0300

    Test: README dosyası güncellendi

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,34 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'toyzzbox.s3.eu-north-1.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "toyzzbox.s3.eu-north-1.amazonaws.com",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {}, // ✅ düzeltildi
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Forwarded-Proto",
+            value: "https",
+          },
+        ],
+      },
+    ];
   },
 };
 
