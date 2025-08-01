b7c79c450c338b49e37b958fa24a34db69344e93
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-01 12:12:29 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/favorites/action.ts
+++ src/app/(admin)/administor/favorites/action.ts
@@ -1,32 +1,26 @@
 "use server";
 
-import { auth } from "@/auth";
+
 import { prisma } from "@/lib/prisma";
 
 export async function toggleFavorite(productId: string) {
-  const session = await auth();
-  console.log("SESSION:", session);       // ✅ Artık user ve id dolu gelmeli
 
-  if (!session?.user?.id) {
-    throw new Error("Giriş yapmalısınız");
-  }
 
-  const userId = session.user.id;
 
   const existing = await prisma.favorite.findUnique({
     where: {
-      userId_productId: { userId, productId },
+      userId_productId: { productId },
     },
   });
 
   if (existing) {
     await prisma.favorite.delete({
-      where: { userId_productId: { userId, productId } },
+      where: { userId_productId: { productId } },
     });
     return { status: "removed" };
   } else {
     await prisma.favorite.create({
-      data: { userId, productId },
+      data: { productId },
     });
     return { status: "added" };
   }
