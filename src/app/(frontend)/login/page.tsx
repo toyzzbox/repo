a178cdf9f747eb4e03c60a7be9e778471807b7e7
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-01 11:56:18 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/login/page.tsx
+++ src/app/(frontend)/login/page.tsx
@@ -1,16 +1,10 @@
 // ✅ Bu satır en üste
 export const dynamic = "force-dynamic";
 
-import { auth } from "@/auth";
 import LoginForm from "@/components/(frontend)/auth/login-form";
-import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
-  const session = await auth(); // ✅ Server-side oturumu getir
 
-  if (session?.user) {
-    redirect("/hesabim"); // ✅ Zaten giriş yaptıysa yönlendir
-  }
 
   return (
     <div className="flex justify-center mt-10">
--- src/app/(frontend)/register/page.tsx
+++ src/app/(frontend)/register/page.tsx
@@ -1,14 +1,7 @@
-import { auth } from "@/auth";
+
 import RegisterForm from "@/components/(frontend)/auth/register-form";
-import { redirect } from "next/navigation";
 
 const RegisterPage = async () => {
-  const session = await auth();
-
-  // ✅ Doğru yaklaşım: session varsa değil, session.user varsa yönlendirme
-  if (session?.user) {
-    redirect("/dashboard");
-  }
 
   // User is not authenticated, show login form
   return (
