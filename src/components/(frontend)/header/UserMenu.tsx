18ea24b241b48d71753ae201dfd9c3a6e5ed0936
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-01 12:01:01 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/header/UserMenu.tsx
+++ src/components/(frontend)/header/UserMenu.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/auth"; // v5 Auth.js'deki server fonksiyon
+
 import { LogOut, Settings, User } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -14,40 +14,20 @@ import {
 import SignOutButton from "../auth/SignOut";
 
 export default async function UserMenu() {
-  const session = await auth();
+
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size="logo" variant={"user"} className="flex items-center gap-2 py-2 px-4 rounded">
         <User />
-          {session?.user?.name?.split(" ")[0] ?? "Giriş Yap"}
+           "Giriş Yap"
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className="w-56 bg-white text-black">
-        {session?.user ? (
-          <>
-            <DropdownMenuLabel>{session.user.email}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem asChild>
-                <Link href="/settings">
-                  <Settings className="mr-2 h-4 w-4" />
-                  <span>Ayarlar</span>
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/api/auth/signout">
-                <LogOut className="mr-2 h-4 w-4" />
-               <SignOutButton/>
-              </Link>
-            </DropdownMenuItem>
-          </>
-        ) : (
-          <>
+      
+         
             <DropdownMenuLabel>Hoş geldiniz</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem asChild>
@@ -56,8 +36,8 @@ export default async function UserMenu() {
             <DropdownMenuItem asChild>
               <Link href="/register">Üye Ol</Link>
             </DropdownMenuItem>
-          </>
-        )}
+          
+      
       </DropdownMenuContent>
     </DropdownMenu>
   );
