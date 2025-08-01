0283d63870735124b7dab23b153fa8db5a8af42e
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-01 11:33:37 +0300

    Test: README dosyası güncellendi

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "https://toyzzbox.com", // ✅ daha güvenli ve env destekli
+  baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "https://toyzzbox.com",
 });
