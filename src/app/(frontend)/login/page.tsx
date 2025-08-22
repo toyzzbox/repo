71c858cb346afef69f0ad2556b682e4737c97e6f
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-22 12:14:08 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/login/page.tsx
+++ src/app/(frontend)/login/page.tsx
@@ -1,11 +1,24 @@
 import LoginForm from "@/components/(frontend)/auth/login-form";
+import { prisma } from "@/lib/prisma";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-// ✅ Bu satır en üste
 export const dynamic = "force-dynamic";
 
-
 const LoginPage = async () => {
+  // Cookie objesini await ile al
+  const cookieStore = await cookies();
+  const token = cookieStore.get("sessionToken")?.value;
 
+  if (token) {
+    const session = await prisma.session.findUnique({
+      where: { token },
+    });
+
+    if (session && session.expiresAt > new Date()) {
+      redirect("/dashboard");
+    }
+  }
 
   return (
     <div className="flex justify-center mt-10">
