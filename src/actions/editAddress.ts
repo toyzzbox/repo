13e0a471d3a93322300d29b499eac574912307d6
toyzzbox <mehmetonurtass02@gmail.com>
2025-08-28 19:14:28 +0300

    Test: README dosyası güncellendi

--- src/actions/editAddress.ts
+++ src/actions/editAddress.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
+import { getSession } from "@/lib/session";
 import { revalidatePath } from "next/cache";
 
 export async function editAddress(_: any, formData: FormData) {
-  const session = await auth();
+  const session = await getSession();
   const userId = session?.user?.id;
   if (!userId) return { success: false };
 
