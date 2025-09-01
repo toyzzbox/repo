532383aeb8c64bd27dbf48948dd68fb92f554a2b
toyzzbox <mehmetonurtass02@gmail.com>
2025-09-01 13:23:53 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,22 +6,32 @@ import PageContainer from "@/components/(frontend)/container/PageContainer";
 import { Toaster } from "sonner";
 import GtagClient from "@/components/(frontend)/analytics/GtagClient";
 import MobileFooter from "@/components/(frontend)/footer/MobileFooter";
-export default function FrontendLayout({ children }: { children: React.ReactNode }) {
+import { getSession } from "@/lib/session"; // Session import edildi
+
+export default async function FrontendLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  // Session'ı server component'te al
+  const session = await getSession();
+
   return (
     <>
       <GtagClient />
       <ReduxProvider>
-          <Toaster position="top-right" richColors />
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">
-              <PageContainer>{children}</PageContainer>
-            </main>
-            <footer>
-              <Footer />
-              <MobileFooter/>
-            </footer>
-          </div>
+        <Toaster position="top-right" richColors />
+        <div className="flex flex-col min-h-screen">
+          {/* Session'ı Header'a prop olarak geç */}
+          <Header session={session} />
+          <main className="flex-1">
+            <PageContainer>{children}</PageContainer>
+          </main>
+          <footer>
+            <Footer />
+            <MobileFooter />
+          </footer>
+        </div>
       </ReduxProvider>
     </>
   );
