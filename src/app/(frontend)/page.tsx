0a64efe6198c19b95b2a7c5ee3716da89185a4b3
toyzzbox <mehmetonurtass02@gmail.com>
2025-09-12 15:25:04 +0300

    Test: README dosyası güncellendi

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
         <Section title="En Popüler Ürünler">
         <CustomCarousel 
           itemsPerView={{
-            mobile: 1,
+            mobile: 2,
             sm: 2,
             md: 3,
             lg: 4,
@@ -58,7 +58,7 @@ export default async function Home() {
       <Section title="En İndirimli Ürünler">
         <CustomCarousel 
           itemsPerView={{
-            mobile: 1,
+            mobile: 2,
             sm: 2,
             md: 3,
             lg: 4,
@@ -79,7 +79,7 @@ export default async function Home() {
       <Section title="En Yeni Ürünler">
         <CustomCarousel 
           itemsPerView={{
-            mobile: 1,
+            mobile: 2,
             sm: 2,
             md: 3,
             lg: 4,
