bea2c1bf43c7c08ec58282c24473fe085c9ff93e
toyzzbox <mehmetonurtass02@gmail.com>
2025-09-12 15:27:53 +0300

    Test: README dosyası güncellendi

--- src/components/(frontend)/container/PageContainer.tsx
+++ src/components/(frontend)/container/PageContainer.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const PageContainer: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="w-full px-4 sm:px-6 md:px-8 lg:px-10 max-w-[1260px] mx-auto overflow-x-hidden">
+    <div className="w-full min-w-[320px] px-4 sm:px-6 md:px-8 lg:px-10 max-w-[1260px] mx-auto overflow-x-hidden">
       {children}
     </div>
   );
