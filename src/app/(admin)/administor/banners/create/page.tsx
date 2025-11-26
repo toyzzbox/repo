2fac086d081c5b618d24a0c66cbe03736eb55278
toyzzbox <mehmetonurtass02@gmail.com>
2025-11-26 13:06:51 +0300

    Test: README dosyası güncellendi

--- src/app/(admin)/administor/banners/create/page.tsx
+++ src/app/(admin)/administor/banners/create/page.tsx
@@ -1,10 +1,10 @@
-import CreateBannerForm from "@/components/admin/banners/CreateBannerForm";
+import CreateBannerForm from "../CreateBannerForm";
 
 export default function CreateBannerPage() {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-6">Yeni Banner Ekle</h1>
-      <CreateBannerForm />
+      <CreateBannerForm/>
     </div>
   );
 }
