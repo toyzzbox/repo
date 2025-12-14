ec468b06118a39205c41c80d3de7b8d85237c14e
toyzzbox <mehmetonurtass02@gmail.com>
2025-12-14 11:13:06 +0300

    Test: README dosyası güncellendi

--- src/actions/google-login.ts
+++ src/actions/google-login.ts
@@ -1,19 +1,15 @@
+"use server";
 
-"use server"
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
-import AuthError  from 'next-auth';
-import {signIn} from "@/auth";
-
-
-export async function googleAutheticate() {
-    try {
-        await signIn('google')
-    } catch (error) {
-        if ( error instanceof AuthError) {
-            return 'google log in failed'
-        }
-        throw error;
-    }  
+export async function googleAuthenticate() {
+  try {
+    await signIn("google");
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return "Google login failed";
+    }
+    throw error;
+  }
 }
-
-
